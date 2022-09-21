8635bd8c7429d7014f774270231a2215c60126b5
qian.guo <qian.guo@xtalpi.com>
2022-09-21 10:14:01 +0800

    feature: 新增关于页面

--- main.go
+++ main.go
@@ -6,10 +6,17 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", handleFunc)
+	http.HandleFunc("/", handleFunc) // "/"意味着任意路径
 	http.ListenAndServe(":3000", nil)
 }
 
 func handleFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>hello, goBlog</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>hello, goBlog</h1>")
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系"+
+			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+	} else {
+		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+"<p>如有疑惑，请联系我们。</p>")
+	}
 }
