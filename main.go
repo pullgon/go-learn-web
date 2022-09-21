2fd1598ec528fdcbe3574797920b7c0fb337f22a
qian.guo <qian.guo@xtalpi.com>
2022-09-21 13:08:39 +0800

    feature: ServeMux

--- main.go
+++ main.go
@@ -6,17 +6,31 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", handleFunc) // "/"意味着任意路径
-	http.ListenAndServe(":3000", nil)
+	router := http.NewServeMux()
+	router.HandleFunc("/", defaultHandler)
+	router.HandleFunc("/about", aboutHandler)
+	router.HandleFunc("/articles/", articleHandler)
+	http.ListenAndServe(":3000", router)
 }
 
-func handleFunc(w http.ResponseWriter, r *http.Request) {
+func articleHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		fmt.Fprint(w, "访问文章列表")
+	case "POST":
+		fmt.Fprint(w, "创建新的文章")
+	}
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html;charset=utf-8")
+	fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系"+
+		"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+}
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>hello, 欢迎来到 goBlog</h1>")
-	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系"+
-			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+"<p>如有疑惑，请联系我们。</p>")
